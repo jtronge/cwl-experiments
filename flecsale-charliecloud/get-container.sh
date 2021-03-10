4dbe67fad9483d891b025d2bf50859a886183700
Jake Tronge <jtronge3@gmail.com>
2021-03-09 19:01:00 -0500

    Add container script contents

--- flecsale-charliecloud/get-container.sh
+++ flecsale-charliecloud/get-container.sh
@@ -1,1 +1,4 @@
-# TODO
+#!/bin/sh
+# Get the Charliecloud container for this workflow
+ch-builder2tar laristra/flecsale:ubuntu_mpi_master .
+mv 'laristra%flecsale:ubuntu_mpi_master.tar.gz' laristra.flecsale.ubuntu_mpi_master.tar.gz
